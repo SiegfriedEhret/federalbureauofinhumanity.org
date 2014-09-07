c47e7360104646b1d3c9317d20e079d0c47472b7
Siegfried Ehret <siegfried@ehret.me>
2014-09-08 01:09:49 +0200

    #duck

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-federalbureauofinhumanity.org
-=============================
+# federalbureauofinhumanity.org
 
-federalbureauofinhumanity.org
+[federalbureauofinhumanity.org](federalbureauofinhumanity.org) site.
+
+Music is licensed under [CREATIVE COMMONS ATTRIBUTION 3.0 UNPORTED LICENSE](http://creativecommons.org/licenses/by/3.0/).
+
+The website is licensed under the [wtfpl](http://wtfpl.org/)
+
+
+               DO WHATEVER THE FUCK YOU WANT, PUBLIC LICENSE
+        TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+        
+                0. You just DO WHATEVER THE FUCK YOU WANT.
+           
---
+++ scripts.js
@@ -0,0 +1,34 @@
+(function() {
+  'use strict';
+  
+  var triggerEn = document.getElementById('toEn');
+  var triggerFr = document.getElementById('toFr');
+  var contentEn = document.querySelectorAll('.en');
+  var contentFr = document.querySelectorAll('.fr');
+
+  function getDisplay(node) {
+    return node && node.nodeName.toLowerCase() === 'div' ? 'block': 'inline-block';
+  }
+  
+  function showEn() {
+    for (var i=0; i<contentEn.length; i++) {
+      contentEn[i].style.display = getDisplay(contentEn[i]);
+    }
+    for (var i=0; i<contentFr.length; i++) {
+      contentFr[i].style.display = 'none';
+    }
+  }
+
+  function showFr() {
+    for (var i=0; i<contentEn.length; i++) {
+      contentEn[i].style.display = 'none';
+    }
+    for (var i=0; i<contentFr.length; i++) {
+      contentFr[i].style.display = getDisplay(contentEn[i]);
+    }
+  }
+  
+  showFr();
+  triggerEn.addEventListener('click', showEn);
+  triggerFr.addEventListener('click', showFr);
+})();
